68a9b5bfcbe6683d84ece03dd59bd53319ecbae1
sabeena1217 <sabeena1217@gmail.com>
2019-02-08 00:08:19 +0530

    adding restrictions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { HeroFormComponent } from './view/hero-form/hero-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroFormComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
